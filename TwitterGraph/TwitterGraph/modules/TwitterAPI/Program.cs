5afc9a51fb8544fdfb43b4bdfc0775ee063c9de4
David Velasco <velascos.davida@gmail.com>
2017-11-22 01:14:41 -0600

    First commit

---
+++ TwitterGraph/TwitterGraph/modules/TwitterAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterAPI {
+    class Program {
+        static void Main(string[] args) {
+
+            TwitterAPI twitterAPI = new TwitterAPI();
+            twitterAPI.doSearchRun();
+            Console.ReadKey();
+        }
+    }
+}
